b846fdd4d8c440e77f6c188a5d927736821be602
Qreitos <Qreitos@github.com>
2023-03-05 20:16:28 +0100

    FIX: fixed constructor problem

--- oranjes/src/main/java/com/orj/oranjes/model/DTO/response/ParentsDto.java
+++ oranjes/src/main/java/com/orj/oranjes/model/DTO/response/ParentsDto.java
@@ -1,6 +1,5 @@
 package com.orj.oranjes.model.DTO.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +7,5 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class ParentsDto {
 }
